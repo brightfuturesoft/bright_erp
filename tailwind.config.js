9e7d0c823bc796097107438a55efcfd1ec64d250
nahid <mdnahid360s@gmail.com>
2024-05-25 05:53:23 -0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Add your custom colors here
+        'primary': '#3498db',
+        'secondary': '#2ecc71',
+        'black': '#111827',
+        'dark': '#1F2937',
+        'light': '#fff',
+        // You can define as many custom colors as you need
+      },
+    },
   },
   plugins: [],
 }
-
