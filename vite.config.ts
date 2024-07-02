45775dcbed78e004ba999553cb8c6eec62a9674b
MH Murshed <murshed1806@gmail.com>
2024-07-02 21:34:24 +0600

    feat: absolute import enabled

--- vite.config.ts
+++ vite.config.ts
@@ -39,20 +39,9 @@ export default defineConfig({
       overlay: true,
     },
   },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  }
 });
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-
-//   plugins: [react()],
-//   server: {
-//     hmr: {
-//       overlay: true,
-//     },
-//   },
-// })
-
-
