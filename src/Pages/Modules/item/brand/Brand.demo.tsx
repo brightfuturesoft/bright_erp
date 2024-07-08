507499ab505b6004b0bc849dcd7f95cabc919e9b
MH Murshed <murshed1806@gmail.com>
2024-07-08 16:24:50 +0600

    feat: brand page added

---
+++ src/Pages/Modules/item/brand/Brand.demo.tsx
@@ -0,0 +1,66 @@
+import { DataType } from '@modules/item/brand/Brand.type';
+
+const tableData: DataType[] = [
+    {
+        key: '123',
+        brand: 'Apple',
+        discount: 'N/A',
+        status: true,
+    },
+    {
+        key: '124',
+        brand: 'Samsung',
+        discount: 'N/A',
+        status: false,
+    },
+    {
+        key: '125',
+        brand: 'Huawei',
+        discount: 'N/A',
+        status: true,
+    },
+    {
+        key: '126',
+        brand: 'Xiaomi',
+        discount: 'N/A',
+        status: true,
+    },
+    {
+        key: '127',
+        brand: 'OnePlus',
+        discount: 'N/A',
+        status: false,
+    },
+    {
+        key: '128',
+        brand: 'Oppo',
+        discount: 'N/A',
+        status: true,
+    },
+    {
+        key: '129',
+        brand: 'Vivo',
+        discount: 'N/A',
+        status: true,
+    },
+    {
+        key: '130',
+        brand: 'LG',
+        discount: 'N/A',
+        status: false,
+    },
+    {
+        key: '131',
+        brand: 'Motorola',
+        discount: 'N/A',
+        status: true,
+    },
+    {
+        key: '132',
+        brand: 'Nokia',
+        discount: 'N/A',
+        status: false,
+    },
+];
+
+export { tableData };
--- src/Pages/Modules/item/brand/Brand.tsx
+++ src/Pages/Modules/item/brand/Brand.tsx
@@ -1,5 +1,24 @@
+import { Button } from 'antd';
+import Section from '@modules/common/components/Section';
+import TableController from '@modules/common/components/TableController';
+import DataTable from '@modules/item/brand/components/DataTable';
+
+const BrandAddButton = () => {
+    return <Button onClick={() => {}}>Add New</Button>;
+};
+
 const Brand = () => {
-    return <div>Brand</div>;
+    return (
+        <div>
+            <Section
+                title="Brands"
+                sideComponent={<BrandAddButton />}
+            >
+                <TableController />
+                <DataTable />
+            </Section>
+        </div>
+    );
 };
 
 export default Brand;
