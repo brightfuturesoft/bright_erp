ec4663f7db06ab983a4db6105ab7a6dd07ec6603
MH Murshed <murshed1806@gmail.com>
2024-07-08 18:24:24 +0600

    feat: size type page added

--- src/Pages/Modules/item/sizeType/SizeType.tsx
+++ src/Pages/Modules/item/sizeType/SizeType.tsx
@@ -1,5 +1,23 @@
+import { Button } from 'antd';
+import Section from '@modules/common/components/Section';
+import TableController from '@modules/common/components/TableController';
+import DataTable from '@modules/item/sizeType/components/DataTable';
+
+const AddBrandButton = () => {
+    return <Button onClick={() => {}}>Add New</Button>;
+};
 const SizeType = () => {
-    return <div>SizeType</div>;
+    return (
+        <div>
+            <Section
+                title="Item Sizes"
+                sideComponent={<AddBrandButton />}
+            >
+                <TableController />
+                <DataTable />
+            </Section>
+        </div>
+    );
 };
 
 export default SizeType;
