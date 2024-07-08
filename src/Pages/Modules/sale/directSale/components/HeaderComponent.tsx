e720afde7d76284207d492c09091828242344995
MH Murshed <murshed1806@gmail.com>
2024-07-09 00:45:52 +0600

    feat: direct sale page added

---
+++ src/Pages/Modules/sale/directSale/components/HeaderComponent.tsx
@@ -0,0 +1,18 @@
+import { Button, Tooltip } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+
+const HeaderComponent = () => {
+    return (
+        <div className="flex gap-2">
+            <Tooltip title="export">
+                <Button
+                    shape="circle"
+                    icon={<DownloadOutlined color="red" />}
+                />
+            </Tooltip>
+            <Button>Add Direct Sale</Button>
+        </div>
+    );
+};
+
+export default HeaderComponent;
