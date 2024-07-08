8b0b156cc9d653f8b087c8b47510a60e3e52727f
MH Murshed <murshed1806@gmail.com>
2024-07-08 18:32:58 +0600

    feat: attribute set page added

---
+++ src/Pages/Modules/item/attributeSet/AttributeSet.type.ts
@@ -0,0 +1,8 @@
+interface DataType {
+    key: string;
+    attributeSet: string;
+    addedAttributes: string;
+    status: boolean;
+}
+
+export type { DataType };
---
+++ src/Pages/Modules/item/attributeSet/components/DataTable.tsx
@@ -0,0 +1,71 @@
+import { Dropdown, Space, Table, TableProps, Tag } from 'antd';
+import { EllipsisVertical } from 'lucide-react';
+import { tableData } from '@modules/item/attributeSet/AttributeSet.demo';
+import { DataType } from '@modules/item/attributeSet/AttributeSet.type';
+
+const items = [
+    {
+        key: '1',
+        label: <div onClick={() => console.log('edit clicked')}>Edit</div>,
+    },
+    {
+        key: '2',
+        label: (
+            <div onClick={() => console.log('inactive clicked')}>
+                Make Inactive
+            </div>
+        ),
+    },
+    {
+        key: '3',
+        label: <div onClick={() => console.log('Delete clicked')}>Delete</div>,
+    },
+];
+
+const tableHead: TableProps<DataType>['columns'] = [
+    {
+        title: 'ATTRIBUTE SET',
+        dataIndex: 'attributeSet',
+        key: 'attributeSet',
+    },
+    {
+        title: 'ADDED ATTRIBUTES',
+        dataIndex: 'addedAttributes',
+        key: 'addedAttributes',
+    },
+    {
+        title: 'STATUS',
+        dataIndex: 'status',
+        key: 'status',
+        render: (status: boolean) => (
+            <Tag color={status ? 'green' : 'red'}>
+                {status ? 'Active' : 'Inactive'}
+            </Tag>
+        ),
+    },
+    {
+        title: 'ACTION',
+        key: 'action',
+        render: () => (
+            <Space size="middle">
+                <Dropdown
+                    menu={{ items }}
+                    trigger={['click']}
+                >
+                    <a>
+                        <EllipsisVertical className="hover:cursor-pointer" />
+                    </a>
+                </Dropdown>
+            </Space>
+        ),
+    },
+];
+
+const DataTable: React.FC = () => (
+    <Table
+        columns={tableHead}
+        dataSource={tableData}
+    />
+);
+
+export default DataTable;
