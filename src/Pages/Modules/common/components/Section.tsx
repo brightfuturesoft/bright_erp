3565c9c767b445c8d8ecb96c6f5dd5b4493106df
MH Murshed <murshed1806@gmail.com>
2024-07-08 16:10:27 +0600

    feat: manufacturers page added

--- src/Pages/Modules/common/components/Section.tsx
+++ src/Pages/Modules/common/components/Section.tsx
@@ -11,7 +11,7 @@ const Section: React.FC<SectionProps> = ({
 }) => {
     return (
         <div>
-            <div>
+            <div className="flex justify-between items-center">
                 <h1 className="font-semibold text-light-dark text-xl dark:text-gray-300">
                     {title}
                 </h1>
--- src/Pages/Modules/item/manufacturers/Manufacturers.tsx
+++ src/Pages/Modules/item/manufacturers/Manufacturers.tsx
@@ -1,5 +1,20 @@
+import { Button } from 'antd';
+import Section from '@modules/common/components/Section';
+import TableController from '@modules/common/components/TableController';
+import DataTable from './components/DataTable';
+
 const Manufacturers = () => {
-    return <div>Manufacturers</div>;
+    return (
+        <div>
+            <Section
+                title="Manufacturers"
+                sideComponent={<Button onClick={() => {}}>Add New</Button>}
+            >
+                <TableController />
+                <DataTable />
+            </Section>
+        </div>
+    );
 };
 
 export default Manufacturers;
